a5487c5a8f9ebe13810af1c4ab8fd53c9dba6adb
Dan <danhboi23@hotmail.com>
2017-07-09 16:24:41 -0700

    Testing module pattern

---
+++ app.js
@@ -0,0 +1,20 @@
+// JS - Module Pattern
+
+// An object 'budgetController' containing the method 'publicTest'
+
+var budgetController = (function() {
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        // This object is what gets assigned to the budgetController after function returns
+        publicTest: function (b) {
+            console.log(add(b));
+        }
+    }
+})();
+
+
