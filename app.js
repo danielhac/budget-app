ea8ff3746701325006caf0eb8b82b0453ca2aeb0
Dan <danhboi23@hotmail.com>
2017-07-23 16:47:54 -0700

    Display current month to go with current year

--- app.js
+++ app.js
@@ -288,11 +288,14 @@ var UIController = (function () {
         },
 
         displayMonth: function () {
-            var now, year;
+            var now, month, year;
             now = new Date();
 
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
             year = now.getFullYear();
-            document.querySelector(DOMstrings.dateLabel).textContent = year;
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
         },
 
         // Making 'DOMstrings' public
